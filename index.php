379aa9647f468c6dc1a6728d32e3d8d3f72052d3
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-12-21 10:24:16 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php  
+
+$name = 'Yoshi';
+
+ echo $name;
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My first php page</title>
+</head>
+<body>
+    
+    <h1>User profile page</h1>
+    <h2><?php  echo $name?> </h2>
+
+</body>
+</html>

f7d009c9dda6fa677657a418f5354de54b1d5066
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-12-22 11:29:45 +0400

    this is some php in html with if statement

--- index.php
+++ index.php
@@ -1,49 +1,35 @@
 <?php  
-    // echo true;
-    // echo false;
 
+$price = 45;
 
-    //numbers
+// if ($price < 30){
+//     echo 'condition is met';
+// } elseif($price < 50){ 
+//     echo 'else if condition';
+// } else {
+//     echo 'condition is not met';
+// }
 
-    // echo 5 < 10;
-    
-    //echo 5 > 10;
+     $products = [
+         ['name' => 'shiny star', 'price' => 20],
+         ['name' => 'green shell', 'price' => 10],
+         ['name' => 'red shell', 'price' => 15],
+         ['name' => 'gold coin', 'price' => 5],
+         ['name' => 'lightning bolt', 'price' => 40],
+         ['name' => 'banana skin', 'price' => 2],
+     ];
 
-    //echo 5 == 10;
+    foreach($products as $product){
+        
+        // if($product['price'] < 15 && $product['price'] > 2){
+        //     echo $product['name'] . '<br />';
+        // }
 
-    //echo 10==10
+        // if($product['price'] > 20 || $product['price'] < 10){
+        //     echo $product['name'] . '<br />';
+        // }
+    }
 
-    //echo 5!= 10;
-
-    //echo  5 <= 5
-
-    //echo 5 >= 5;
-
-    //strings
-
-    //echo 'shaun' < 'yoshi';
-
-    //echo 'shaun' > 'yoshi';
-
-    // echo 'shaun' > 'Shaun';
-
-    // echo 'mario' == 'mario';
-
-    // echo 'mario' == 'Mario';
-
-
-    // loose vs strict comparison
-
-    // echo 5 == '5';
-
-    // echo 5 === '5';
-
-    // echo 5==5;
-    // echo 5 === 5;
-
-    // echo true == '1';
-
-    echo false == '';
 
 ?>
 
@@ -53,6 +39,20 @@
     <title>My first php page</title>
 </head>
 <body>
+
+<div>
+    <ul>
+        <?php foreach($products as $product){ ?>
+            <?php if($product ['price'] > 15){ ?>
+                <li><?php echo $product['name']?></li>
+            <?php } ?>
+
+
+        <?php } ?>
+
+    </ul>
+</div>
+
 
 </body>
 </html>

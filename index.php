7d0674f899e6352c4d8fec187913f0cd5711e3a5
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-12-22 16:27:03 +0400

    some php page starting up

--- index.php
+++ index.php
@@ -1,24 +1,16 @@
 <?php  
-    // include('ninjas.php');
-    // require('ninjas.php');
-
-    require('ninjas.php');
 
 
-    echo 'end of php'
 
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>My first php page</title>
-</head>
+
 <body>
 
-    <?php  include('content.php'); ?>
-    <?php  include('content.php'); ?>
-    <?php  include('content.php'); ?>
+    <?php include('template/header.php') ?>
+    <?php include('template/footer.php') ?>
 
 </body>
 </html>
--- ninjas.php
+++
@@ -1,5 +0,0 @@
-<?php  
-    $ninjas = ['shaun', 'ruy', 'yoshi'];
-    echo $ninjas[1]. '<br />';
-
-?>

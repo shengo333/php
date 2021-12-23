2cc3b869ba4f7d77297e9465c93a2b9c751bae85
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-12-23 12:25:36 +0400

    some validation forms

--- add.php
+++ add.php
@@ -1,5 +1,10 @@
 <?php  
 
+
+$title =  '';
+$email = '';
+$ingredients = '';
+
 $errors = array('email' => '', 'title'=> '', 'ingredients'=> '');
 
 if(isset($_POST['submit'])){
@@ -47,13 +52,13 @@ if(isset($_POST['submit'])){
     <h4 class="center">Add a Pizza</h4>
     <form action="add.php" method="POST" class="white">
         <label>Your email:</label>
-        <input type="text" name="email">
+        <input type="text" name="email" value="<?php echo htmlspecialchars($email)?>">
         <div class="red-text"> <?php echo $errors['email']?> </div>
         <label>Pizza title:</label>
-        <input type="text" name="title">
+        <input type="text" name="title" value="<?php echo htmlspecialchars($title)?>">
         <div class="red-text"><?php echo $errors['title']?></div>
         <label>Ingredients comma separated:</label>
-        <input type="text" name="ingredients">
+        <input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients)?>">
         <div class="red-text"><?php echo $errors['ingredients']?></div>
         <div class="center">
             <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">

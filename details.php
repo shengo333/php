2453bf6b52e5454b605124d216813fb994c681a3
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-12-25 13:03:55 +0400

    delete button

--- details.php
+++ details.php
@@ -1,28 +1,39 @@
 <?php 
 
 include('config/db_connect.php');
+    if(isset($_POST['delete'])){
+
+		$id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+		$sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+		if(mysqli_query($conn, $sql)){
+			header('Location: index.php');
+		} else {
+			echo 'query error: '. mysqli_error($conn);
+		}
+
+	}
 
 // check GET  request parameters
 if(isset($_GET['id'])){
 
     $id= mysqli_real_escape_string($conn, $_GET['id']);
-}
-
-//make sql
-
-    $sql ="SELECT * FROM pizzas WHERE id= $id";
-
-// get queri result
-    $result = mysqli_query($conn,$sql);
-
+    
+    //make sql
+    $sql = "SELECT * FROM pizzas WHERE id= $id";
+    
+    // get queri result
+    $result = mysqli_query($conn, $sql);
+    
     //fetch result in array format
-
+    
     $pizza = mysqli_fetch_assoc($result);
-
+    
     mysqli_free_result($result);
     mysqli_close($conn);
-
-    print_r($pizza)
+    
+}
 
 ?>
 
@@ -34,14 +45,21 @@ if(isset($_GET['id'])){
 
 <div class="container center">
     <?php if($pizza): ?>
-        <h4><?php echo htmlspecialchars($pizza['title']);?></h4>
-        <p>Created by: <?php echo htmlspecialchars($pizza['email']);?></p>
-        <p> <?php echo date($pizza['created_at']);?></p>
-        <h5>Ingridients:</h5>
-        <p><?php echo htmlspecialchars($pizza['ingredients']) ?></p>
-    <?php else: ?> 
-        <h5>No such Pizza exists</h5>
-    <?php endif ?>
+			<h4><?php echo $pizza['title']; ?></h4>
+			<p>Created by <?php echo $pizza['email']; ?></p>
+			<p><?php echo date($pizza['created_at']); ?></p>
+			<h5>Ingredients:</h5>
+			<p><?php echo $pizza['ingredients']; ?></p>
+
+			<!-- DELETE FORM -->
+			<form action="details.php" method="POST">
+				<input type="hidden" name="id_to_delete" value="<?php echo $pizza['id']; ?>">
+				<input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+			</form>
+
+		<?php else: ?>
+			<h5>No such pizza exists.</h5>
+		<?php endif ?>
 
 
 </div>

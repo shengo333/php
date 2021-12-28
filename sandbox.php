1cab397640c81cd9004c30822deea98ae0da0f66
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2021-12-28 05:40:01 +0400

    cookies

--- sandbox.php
+++ sandbox.php
@@ -1,6 +1,11 @@
 <?php 
 
     if(isset($_POST['submit'])){
+
+        // cookie for gender
+
+        setcookie('gender', $_POST['gender'],time() + 86400);
+
         session_start();
         $_SESSION['name'] = $_POST['name'];
 
@@ -18,6 +23,10 @@
 <body>
 	<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST"> 	
         <input type="text" name="name">
+        <select name="gender">
+            <option value="male">male</option>
+            <option value="female">female</option>
+        </select>
         <input type="submit" name="submit" value="submit">
     </form>
 
--- template/header.php
+++ template/header.php
@@ -3,6 +3,7 @@
     session_start();
 
     
+    
     //$_SESSION['name'] = 'yoshi';
 
     if($_SERVER['QUERY_STRING'] =='noname'){
@@ -11,6 +12,10 @@
     }
 
     $name = $_SESSION['name'] ?? 'Guest';
+
+    //get cookie
+
+    $gender = $_COOKIE['gender'] ?? 'unknown';
 
 
 ?>
@@ -48,6 +53,7 @@
             <a href="index.php" class="brand-logo brand-text">Ninja Pizza</a>
             <ul id="nav-mobile" class="right hide-on-small-down">
                 <li class="grey-text">Hello <?php echo htmlspecialchars($name)?></li>
+                <li class="grey-text"> (<?php echo htmlspecialchars($gender)?>)</li>
                 <li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
             </ul>
         </div>
